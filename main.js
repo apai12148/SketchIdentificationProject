5347befeec1e17a0b87ee43f6b12a7eb70ec70d1
apai12148 <97473664+apai12148@users.noreply.github.com>
2023-04-19 20:17:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,5 +8,43 @@ document.getElementById("sketch_draw").innerHTML = "Sketch to be Drawn - " + ske
 timer_counter = 0
 timer_check = ""
 drawn_sketch = ""
-anwer_holder = ""
+answer_holder = ""
 score = 0
+
+updateCanvas()
+{
+    background("white");
+}
+
+function setup(){
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function draw(){
+check_sketch()
+if(drawn_sketch==sketch){
+    answer_holder = "set"
+    score++
+    document.getElementById("score").value = "Score: " + score;
+} else{
+
+}
+}
+
+check_sketch()
+{
+timer_counter++
+document.getElementById("timer").value = "Timer" + timer_counter
+console.log(timer_counter);
+if(timer_counter>400){
+    timer_counter = 0
+    timer_check = "completed"
+}
+if(timer_check == "completed"){
+    timer_check = ""
+    answer_holder = ""
+    updateCanvas()
+}
+}
