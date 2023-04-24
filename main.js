f0006f3bddff549ad642e3b3256c1f2e8fe43412
apai12148 <97473664+apai12148@users.noreply.github.com>
2023-04-24 19:41:57 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,40 +11,70 @@ drawn_sketch = ""
 answer_holder = ""
 score = 0
 
-updateCanvas()
+function updateCanvas()
 {
     background("white");
+    random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1)
+console.log(random_no);
+sketch = quick_draw_data_set[random_no];
+document.getElementById("sketch_draw").innerHTML = "Sketch to be Drawn - " + sketch
 }
 
 function setup(){
     canvas = createCanvas(280, 280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
 }
 
 function draw(){
+    strokeWeight(13);
+    stroke(0)
+    if(mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
 check_sketch()
 if(drawn_sketch==sketch){
     answer_holder = "set"
     score++
     document.getElementById("score").value = "Score: " + score;
-} else{
-
 }
 }
 
-check_sketch()
+function check_sketch()
 {
 timer_counter++
-document.getElementById("timer").value = "Timer" + timer_counter
+document.getElementById("timer").innerHTML = "Timer" + timer_counter
 console.log(timer_counter);
 if(timer_counter>400){
     timer_counter = 0
     timer_check = "completed"
 }
-if(timer_check == "completed"){
+if(timer_check == "completed" || answer_holder == "set"){
     timer_check = ""
     answer_holder = ""
     updateCanvas()
 }
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    console.log(results);
+
+    document.getElementById('your_sketch').innerHTML = 'Your Sketch ' + drawn_sketch;
+
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
